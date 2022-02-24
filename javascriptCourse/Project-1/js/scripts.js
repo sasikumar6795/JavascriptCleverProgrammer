d3343af27690a51dc75579ffd799b8ca69ef68e6
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2022-02-24 21:40:12 +0530

    Picked card randomly

--- javascriptCourse/Project-1/js/scripts.js
+++ javascriptCourse/Project-1/js/scripts.js
@@ -257,6 +257,19 @@ let blackJackGame = {
         'div': '#dealer-box',
         'score': 0
     },
+    'card' :['2','3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        'K',
+        'Q',
+        'J',
+        'A',
+    ],
 
 };
 
@@ -266,17 +279,42 @@ const DEALER=blackJackGame['dealer'];
 const hitSound= new Audio('./sounds/swish.m4a');
 
 document.querySelector('#blackjack-hit-button').addEventListener('click',blackJackHit);
+document.querySelector('#blackjack-deal-button').addEventListener('click',blackJackDeal);
 
 function blackJackHit()
 {
-    showCard();
+    let card =randomCard();
+    console.log(card);
+    showCard(card,YOU);
 }
 
-function showCard()
+function showCard(card,activePlayer)
 {
     let cardImage=document.createElement('img');
-    cardImage.src='./images/Q.png';
-    document.querySelector(YOU['div']).appendChild(cardImage);
+    cardImage.src=`./images/${card}.png`;
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
 
     hitSound.play();
+}
+
+function blackJackDeal()
+{
+    let yourImages=document.querySelector("#your-box").querySelectorAll("img");
+    let dealerImages=document.querySelector("#dealer-box").querySelectorAll("img");
+
+    for(i=0;i<yourImages.length;i++)
+    {
+        yourImages[i].remove();
+    }
+
+    for(i=0;i<dealerImages.length;i++)
+    {
+        dealerImages[i].remove();
+    }
+}
+
+function randomCard()
+{
+   let randomIndex= Math.floor(Math.random()*13);
+  return blackJackGame['card'][randomIndex];
 }
